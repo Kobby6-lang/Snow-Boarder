26fee0446bd1b3843bd86caddc508d7bc6a8f4bd
Kobby6-lang <koseitutu43@gmail.com>
2023-07-24 11:13:37 +0100

    Snow

---
+++ Assets/Scripts/CrashDetector.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CrashDetector : MonoBehaviour
+{
+    [SerializeField] float restartDelay = 2f;
+    [SerializeField] ParticleSystem crashEffect;
+    [SerializeField] AudioClip CrashSFX;
+
+    bool hasCrashed = false;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Ground" && !hasCrashed)
+        {
+            hasCrashed = true;
+            FindObjectOfType<PlayerController>().DisableControls();
+            crashEffect.Play();
+            GetComponent<AudioSource>().PlayOneShot(CrashSFX);
+            Invoke("ReloadScene", restartDelay);  
+        }
+    }
+
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
